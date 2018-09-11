d6b6dc5321dfec52eccff5e86b5d525dfb7012a6
SparksFullStack <csparks@antioch.edu>
2018-09-11 13:37:02 -0700

    added a test that removes a user from the database

--- backend/src/tests/model_tests/user_model_tests/user_model_tests.js
+++ backend/src/tests/model_tests/user_model_tests/user_model_tests.js
@@ -1,23 +1,31 @@
 const assert = require('assert');
 const UserModel = require('../../..//models/UserModel');
 
+// ** OPTIONAL: Make the testing less repetitive with declaring newUser in each test
+// ** OPTIONAL: Add .catch handlers for catching errors
 describe('Testing the User Models on the DB', () => {
     // initialize the user so it can be accessed in all of the It Blocks
-    let newUser;
 
     // creating an new user before each test
-    beforeEach((done) => {
-        newUser = new UserModel({
-            fullname: 'MacGruber',
-            username: 'Doobs',
-            email: 'grubes@grubes.com',
-            password: 'thisisapassword'
-        });
-        done();
-    });
+    // beforeEach((done) => {
+    //     newUser = new UserModel({
+    //         fullname: 'macgruber',
+    //         username: 'doobs',
+    //         email: 'grubes@grubes.com',
+    //         password: 'thisisapassword'
+    //     });
+    //     done();
+    // });
     
     // saves the user model, then makes sure it is stored properly
     it('adds a new user to the database', (done) => {
+        const newUser = new UserModel({
+            fullname: 'user1',
+            username: 'user1',
+            email: 'user1@user.com',
+            password: 'user1'
+        });
+
         newUser.save()
             .then(() => {
                 UserModel.findOne({ email: newUser.email })
@@ -27,4 +35,26 @@ describe('Testing the User Models on the DB', () => {
                     })
             })
     });
+
+    // saves a new user model then makes sure it can be removed
+    it('adds and removes a new user from the database', (done) => {
+        const newUser = new UserModel({
+            fullname: 'user2',
+            username: 'user2',
+            email: 'user2@user.com',
+            password: 'user2'
+        });
+
+        newUser.save()
+            .then(() => {
+                newUser.remove()
+                    .then(({ username }) => { // destructuring the username prop off of the deleted record
+                        UserModel.find({ username })
+                            .then(response => {
+                                assert(response.length === 0);
+                                done();
+                            })
+                    })
+            })         
+    })
 })
