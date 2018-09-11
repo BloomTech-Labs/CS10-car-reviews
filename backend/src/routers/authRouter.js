f3dc77e0022481df88a256a17b4516063b94e6f2
SparksFullStack <csparks@antioch.edu>
2018-09-10 17:36:04 -0700

    minor changes made to authRouter to make it async

--- backend/src/routers/authRouter.js
+++ backend/src/routers/authRouter.js
@@ -17,27 +17,33 @@ const router = express.Router();
 // creating the routes
 router.get('/', (req, res) => res.send('The auth router is working!')); // test route
 
+
+// route registers a user
 // * TODO: Need to check that email is in the correct format
 // ** OPTIONAL: Could externalize the form checking with a custom middleware
+// ** OPTIONAL: Modify the payload to hold more/other data
 router.post('/register', (req, res) => { // route that handles registering a user
     const { fullname, username, email, password } = req.body;
     const newUser = new UserModel({ fullname, username, email, password });
     
     // the UserModel has a pre-save hook on it that hashes the password with Bcrypt
     newUser.save(err => {
-        if (err) res.status(500).send(`There was a server error when trying to register the user--please try again.`);
-        else {
-            const newJWT = JWT.sign({ username, email, password }, JWT_SECRET);
-            res.status(200).json({ JWT: newJWT });
-        }
+        if (err) return res.status(500).json({ registerError: `An account with those credentials already exists, please sign in.` });
+        
+        // NOTE: JWTs are signed with the fullname, username, and email as the payload
+        JWT.sign({ fullname, username, email }, JWT_SECRET, (err, token) => {
+            if (err) return res.status(500).json({ registerError: `There was an error when trying to generate a JWT for the user--please try again.`});
+            res.status(200).json({ JWT: token });
+        })
     });
 });
 
+// route logs in a user
 // ** OPTIONAL: Create a middleware that handles the issuing of a JWT
 router.post('/login', (req, res) => {
     const { username, email, password } = req.body;
     // ** OPTIONAL: Find a way to structure conditional so user can enter email or username
-    if (!email || !password) return res.status(400).send({loginError: 'Please provide a username or email and a password to login.'});
+    if (!email || !password) return res.status(400).send({ loginError: 'Please provide a username or email and a password to login.' });
 
     UserModel.findOne({ email: email })
         .then(userRecord => {
@@ -45,14 +51,14 @@ router.post('/login', (req, res) => {
             
             // ** OPTIONAL: Add a password reset feature
             if (bcrypt.compareSync(password, userRecord.password)){
-                const newJWT = JWT.sign({ username, email, password }, JWT_SECRET);
-                res.status(200).json({ JWT: newJWT });
-            } else {
-                res.status(401).json({ loginError: `The password you provided didn't match the one stored in our database, please try again`})
-            }
-            
+                JWT.sign({ username, email, password }, JWT_SECRET, (err, decoded) => {
+                    if (err) return res.status(500).json({ loginError: `There was an error when trying to generate a JWT for the user--please try again.` });
+                    res.status(200).json({ JWT: decoded });
+                });
+            } else res.status(401).json({ loginError: `The password you provided didn't match the one stored in our database, please try again` });
         })
 })
+
 
 // exporting the router
 module.exports = router;
--- backend/src/routers/contentRouter.js
+++ backend/src/routers/contentRouter.js
@@ -1,10 +1,25 @@
 // import dependencies
 const express = require('express');
 
+// destructuring our secret
+const { JWT_SECRET } = process.env;
+
+// importing our models
+const UserModel = require('../models/UserModel');
+const ReviewModel = require('../models/ReviewModel');
+const CarModel = require('../models/CarModel');
+
+const homeRouter = require('./content/homeRouter');
+
 // initializing the server
 const router = express.Router();
 
 // creating the routes
-router.get('/', (req, res) => res.send(`The content router is working!`));
+router.get('/', (req, res) => res.send(`The content router is working!`)); // test route
 
+// using the sub-routers
+router.use('/homepage', homeRouter);
+
+
+// exporting the router
 module.exports = router;
