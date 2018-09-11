647789dacfd337325e33131e25a09961b5c15a64
SparksFullStack <csparks@antioch.edu>
2018-09-11 13:05:30 -0700

    added first unit test for saving the UserModel

---
+++ backend/src/tests/model_tests/user_model_tests/model_tests_helper.js
@@ -0,0 +1,26 @@
+// NOTE: This file is used to initialize a connection to the server before any tests run and externalize other functionality
+// here we setup and initialize Mongoose just like we would normally:
+const mongoose = require('mongoose');
+
+// configuring the database
+mongoose.Promise = global.Promise
+const databaseOptions = {
+    useNewUrlParser: true, // mongoose's URL parser is also deprecated, so we pass this in as a option to use the new one
+};
+mongoose.set('useCreateIndex', true); // collection.ensureIndex is also deprecated so we use 'useCreateIndex' instead
+
+// connects to the database before the tests start
+before(done => {
+    mongoose.connect('mongodb://localhost:27017/store', databaseOptions);
+    mongoose.connection
+        .once('open', () => done()) // here no test will run until done() is executed
+        .on('error', (err) => console.warn(`There was an error connecting to the database: \n${err}`));
+});
+
+// drops the users database before every test file runs
+beforeEach(done => {
+    mongoose.connection.collections.users.drop(() => {
+        console.log(`The database was dropped`);
+        done();
+    })
+})
---
+++ backend/src/tests/model_tests/user_model_tests/user_model_tests.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const UserModel = require('../../..//models/UserModel');
+
+describe('Testing the User Models on the DB', () => {
+    // initialize the user so it can be accessed in all of the It Blocks
+    let newUser;
+
+    // creating an new user before each test
+    beforeEach((done) => {
+        newUser = new UserModel({
+            fullname: 'MacGruber',
+            username: 'Doobs',
+            email: 'grubes@grubes.com',
+            password: 'thisisapassword'
+        });
+        done();
+    });
+
+    // removing the new user after each test
+    afterEach((done) => {
+        newUser.remove()
+            .then(() => done());
+    })
+    
+    // saves the user model, then makes sure it is stored properly
+    it('adds a new user to the database', (done) => {
+        newUser.save()
+            .then(() => {
+                UserModel.findOne({ email: newUser.email })
+                    .then(userRecord => {
+                        assert(userRecord.username === newUser.username)
+                        done();
+                    })
+            })
+    })
+})
