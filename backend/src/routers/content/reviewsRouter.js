fbcf094f8e20e826e7b4a37d0dd256221fbb3e26
Steve <shalomlandsman@gmail.com>
2018-09-12 15:57:45 -0400

    add update users collection when post to reviews

--- backend/src/routers/content/reviewsRouter.js
+++ backend/src/routers/content/reviewsRouter.js
@@ -3,6 +3,7 @@
 // importing dependencies
 const express = require('express');
 const ReviewModel = require('../../models/ReviewModel');
+const UserModel = require('../../models/UserModel')
 
 // intializing the router
 const router = express.Router();
@@ -12,14 +13,21 @@ const router = express.Router();
 router.get('/', (req, res) => res.send(`The reviews router is working!`)); // test router
 
 router.post('/', (req, res) => {
+    let newReviewID;
     const { car, user, content, score } = req.body;
+    const { userId } = req.body.user;
     if (!user || !content || !score) {
         res.status(400).json({ errorMessage: "Please provide user, review, and score." })
         return;
     }
     ReviewModel.create(req.body)
-        .then(result => res.status(201).json(result))
-        .catch(err => res.status(500).json({ error: err.message }));
+        .then(result => {
+             res.status(201).json(result);
+             newReviewID = result._id;
+             return result;
+             })
+        .then(newReviewID => UserModel.findOneAndUpdate(userId, { "$push": { reviews: newReviewID }})) // adds review id to the user document of the author
+        .catch(err => res.status(500).json({ error: err.message }))
 });
 
 // exporting the router
