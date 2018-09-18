edab0e2d16f25b8c96aeee8df21f525a1d7fbfa6
Herbert Hodgson <herberthodgson@hotmail.com>
2018-09-18 18:08:53 -0400

    POST functionality for reviews in newreview.js works.
    - GET call in CDM -> reviewlist.js, newreview.js for user data

--- car-reviews/src/components/Modals/newreview.js
+++ car-reviews/src/components/Modals/newreview.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import { DropdownToggle, DropdownMenu, DropdownItem, UncontrolledDropdown } from 'reactstrap';
 import placeholder from '../../logo.svg';
+import axios from 'axios';
 
 class NewReviewModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      userInfo: {},
       modal: false,
-      selectedImage: undefined,
-      content: ''
+      review: {
+        year: Number(''),
+        make: '',
+        model: '',
+        edition: '',
+        // selectedImage: undefined,
+        content: '',
+        score: Number('')
+      }
     };
   }
+
+  componentDidMount = () => {
+    axios
+      .get('https://back-lambda-car-reviews.herokuapp.com/api/users/data', {
+        headers: {
+          JWT: localStorage.getItem('jwt')
+        }
+      })
+      .then(response => {
+        console.log(response.data);
+        this.setState({ userInfo: response.data });
+      })
+      .catch(console.log('Error getting user data'));
+  };
 
   toggle = () => {
     this.setState({
@@ -20,16 +42,47 @@ class NewReviewModal extends Component {
   };
 
   imageSelectedHandler = event => {
-    this.setState({
-      selectedImage: event.target.files[0]
-    });
+    // this.setState({
+    //   selectedImage: event.target.files[0]
+    // });
   };
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+  handleChange = (type, field) => event => {
+    const newState = Object.assign({}, this.state);
+    newState[type][field] = event.target.value;
+    this.setState(newState);
   };
 
-  handleSubmit = () => {};
+  submitNewReview = () => {
+    const newReview = this.state['review'];
+    const requestURL = 'https://back-lambda-car-reviews.herokuapp.com/api/reviews';
+    axios
+      .post(requestURL, newReview, {
+        headers: {
+          JWT: localStorage.getItem('jwt')
+        }
+      })
+      .then(response => {
+        console.log(response);
+        this.setState({
+          review: {
+            year: '',
+            make: '',
+            model: '',
+            edition: '',
+            // selectedImage: undefined,
+            content: '',
+            score: ''
+          }
+        });
+      })
+      .catch(err => console.warn(err));
+  };
+
+  onClick = event => {
+    this.submitNewReview();
+    this.toggle();
+  };
 
   render() {
     return (
@@ -39,78 +92,65 @@ class NewReviewModal extends Component {
         </button>
         <Modal isOpen={this.state.modal} toggle={this.toggle}>
           <ModalHeader toggle={this.toggle}>
-            <div className="searchfields">
-              <UncontrolledDropdown className="dropdownsModal">
-                <DropdownToggle caret>Year</DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem header>TODO:</DropdownItem>
-                  <DropdownItem disabled>map years onto here</DropdownItem>
-                  <DropdownItem>2000</DropdownItem>
-                  <DropdownItem>2001</DropdownItem>
-                  <DropdownItem>2002</DropdownItem>
-                  <DropdownItem>2003</DropdownItem>
-                  <DropdownItem>2004</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Makes will be dependent on Year -> </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <UncontrolledDropdown className="dropdownsModal">
-                <DropdownToggle caret>Make</DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem header>TODO:</DropdownItem>
-                  <DropdownItem disabled>map makes onto here</DropdownItem>
-                  <DropdownItem>Ford</DropdownItem>
-                  <DropdownItem>Toyota</DropdownItem>
-                  <DropdownItem>Honda</DropdownItem>
-                  <DropdownItem>BMW</DropdownItem>
-                  <DropdownItem>Volkswagon</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Models are dependent on Make -> </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <UncontrolledDropdown className="dropdownsModal">
-                <DropdownToggle caret>Model</DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem header>TODO:</DropdownItem>
-                  <DropdownItem disabled>map models onto here</DropdownItem>
-                  <DropdownItem>Camery</DropdownItem>
-                  <DropdownItem>Corolla</DropdownItem>
-                  <DropdownItem>Prius</DropdownItem>
-                  <DropdownItem>Rav4</DropdownItem>
-                  <DropdownItem>Tundra</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>these examples are if someone picked toyota</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <UncontrolledDropdown className="dropdownsModal">
-                <DropdownToggle caret>Trim</DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem header>TODO:</DropdownItem>
-                  <DropdownItem disabled>map trims onto here</DropdownItem>
-                  <DropdownItem>One</DropdownItem>
-                  <DropdownItem>Two</DropdownItem>
-                  <DropdownItem>Two Eco</DropdownItem>
-                  <DropdownItem>Three</DropdownItem>
-                  <DropdownItem>Three Touring</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>these examples are if someone picked Prius</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </div>
-            Review by: Reviewer
+            <input
+              type="text"
+              name="year"
+              value={this.state.review.year}
+              onChange={this.handleChange('review', 'year')}
+              placeholder="car-year"
+            />
+            <input
+              type="text"
+              name="make"
+              value={this.state.review.make}
+              onChange={this.handleChange('review', 'make')}
+              placeholder="car-make"
+            />
+            <input
+              type="text"
+              name="model"
+              value={this.state.review.model}
+              onChange={this.handleChange('review', 'model')}
+              placeholder="car-model"
+            />
+            <input
+              type="text"
+              name="edition"
+              value={this.state.review.edition}
+              onChange={this.handleChange('review', 'edition')}
+              placeholder="car-edition"
+            />
+            {/* <div className="searchfields">
+              <select className="dropdownsNR" name="car-years" id="car-years" />
+              <select className="dropdownsNR" name="car-makes" id="car-makes" />
+              <select className="dropdownsNR" name="car-models" id="car-models" />
+              <select className="dropdownsNR" name="car-model-trims" id="car-model-trims" />
+            </div> */}
+            Review by: {this.state.userInfo.username}
           </ModalHeader>
           <ModalBody>
-            {this.selectedImage ? <img src={this.selectedImage} /> : <img src={placeholder} />}
+            {this.state.review.selectedImage ? (
+              <img src={this.state.review.selectedImage} />
+            ) : (
+              <img src={placeholder} />
+            )}
             {/* <img style={{ height: '160px', width: '320px' }} /> */}
             <input
               type="file"
               name="selectedImage"
-              value={this.selectedImage}
+              value={this.state.review.selectedImage}
               onChange={this.imageSelectedHandler}
             />
           </ModalBody>
           <ModalFooter>
             <form>
+              <input
+                type="text"
+                name="score"
+                value={this.state.review.score}
+                onChange={this.handleChange('review', 'score')}
+                placeholder="car-score"
+              />
               <p>
                 <textarea
                   className="contentInput"
@@ -118,13 +158,13 @@ class NewReviewModal extends Component {
                   cols="50"
                   placeholder="Write your review here..."
                   name="content"
-                  value={this.state.content}
-                  onChange={this.handleChange}
+                  value={this.state.review.content}
+                  onChange={this.handleChange('review', 'content')}
                 />
               </p>
-              <input type="submit" />
             </form>
           </ModalFooter>
+          <input type="submit" onClick={this.onClick} />
         </Modal>
       </div>
     );
