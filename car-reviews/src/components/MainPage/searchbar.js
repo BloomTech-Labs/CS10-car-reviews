492815ce5399aece69e12444971610ab396556ca
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-09-18 09:48:59 -0700

    dropdowns working

--- car-reviews/src/components/MainPage/searchbar.js
+++ car-reviews/src/components/MainPage/searchbar.js
@@ -47,71 +47,11 @@ class Searchbar extends React.Component {
     return (
         <div className="searchbar">
             {this.handleRenderSignin()}
-            <div className="searchfields">
-                <UncontrolledDropdown className="dropdowns">
-                    <DropdownToggle caret>
-                        Year
-                    </DropdownToggle>
-                    <DropdownMenu>
-                        <DropdownItem header>TODO:</DropdownItem>
-                        <DropdownItem disabled>map years onto here</DropdownItem>
-                        <DropdownItem>2000</DropdownItem>
-                        <DropdownItem>2001</DropdownItem>
-                        <DropdownItem>2002</DropdownItem>
-                        <DropdownItem>2003</DropdownItem>
-                        <DropdownItem>2004</DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem>Makes will be dependent on Year -> </DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-                <UncontrolledDropdown className="dropdowns">
-                    <DropdownToggle caret>
-                        Make
-                    </DropdownToggle>
-                    <DropdownMenu>
-                        <DropdownItem header>TODO:</DropdownItem>
-                        <DropdownItem disabled>map makes onto here</DropdownItem>
-                        <DropdownItem>Ford</DropdownItem>
-                        <DropdownItem>Toyota</DropdownItem>
-                        <DropdownItem>Honda</DropdownItem>
-                        <DropdownItem>BMW</DropdownItem>
-                        <DropdownItem>Volkswagon</DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem>Models are dependent on Make -> </DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-                <UncontrolledDropdown className="dropdowns">
-                    <DropdownToggle caret>
-                        Model
-                    </DropdownToggle>
-                    <DropdownMenu>
-                        <DropdownItem header>TODO:</DropdownItem>
-                        <DropdownItem disabled>map models onto here</DropdownItem>
-                        <DropdownItem>Camery</DropdownItem>
-                        <DropdownItem>Corolla</DropdownItem>
-                        <DropdownItem>Prius</DropdownItem>
-                        <DropdownItem>Rav4</DropdownItem>
-                        <DropdownItem>Tundra</DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem>these examples are if someone picked toyota</DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-                <UncontrolledDropdown className="dropdowns">
-                    <DropdownToggle caret>
-                        Trim
-                    </DropdownToggle>
-                    <DropdownMenu>
-                        <DropdownItem header>TODO:</DropdownItem>
-                        <DropdownItem disabled>map trims onto here</DropdownItem>
-                        <DropdownItem>One</DropdownItem>
-                        <DropdownItem>Two</DropdownItem>
-                        <DropdownItem>Two Eco</DropdownItem>
-                        <DropdownItem>Three</DropdownItem>
-                        <DropdownItem>Three Touring</DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem>these examples are if someone picked Prius</DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
+            <div classname="searchfields">
+                <select className="dropdowns" name="car-years" id="car-years"></select>
+                <select className="dropdowns" name="car-makes" id="car-makes"></select> 
+                <select className="dropdowns" name="car-models" id="car-models"></select>
+                <select className="dropdowns" name="car-model-trims" id="car-model-trims"></select>
             </div>
             <div className="review-and-search">
                 <Link to= {
