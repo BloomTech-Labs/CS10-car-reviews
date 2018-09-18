3abf6082a2ba23a0ac6ebbe09218c910aae7c592
SparksFullStack <csparks@antioch.edu>
2018-09-18 12:34:13 -0700

    added password changing support to usersettings.js

--- car-reviews/src/components/UserPage/usersettings.js
+++ car-reviews/src/components/UserPage/usersettings.js
@@ -1,84 +1,103 @@
 import React, { Component } from 'react';
+import {
+  Card, 
+  CardTitle,
+  CardText,
+  CardBody,
+  Label,
+  Button
+} from 'reactstrap';
+import axios from 'axios';
 import './usersettings.css';
+
+// * TODO: Center vertically
+const styles = {
+  cardStyles: {
+    width: "30%",
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  inputStyles: {
+    width: '80%',
+    marginTop: 0,
+  },
+  labelStyles: {
+    margin: 0,
+    textAlign: 'left',
+  },
+  inputGroupStyles: {
+    marginBottom: "5%",
+  }
+}
 
 // A form component that will display the name and email of a logged in user and allow them to change them and/or their password. Renders within the Settings component.
 
+// * TODO: Add a confirmation that the user has had their password changed successfully
 class UserSettings extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '', // should be set to the username of the logged in user
-      email: '', // should be set to the email of the logged in user
-      oldpassword: '', // should be set to the password on file for logged in user
-      newpassword: ''
+      password: '',
+      password2: ''
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+  handleChange = (passType) => (event) =>{
+    event.preventDefault();
+    this.setState({ [passType]: event.target.value });
   }
 
-  handleSubmit() {}
+  handleSubmit = (event) => {
+    const { password } = this.state;
+    const config = {
+      headers: { 'jwt': localStorage.getItem('jwt') }
+    };
+    event.preventDefault();
+    const localRequests = 'http://localhost:3001/api/users/data'
+    axios.put(localRequests, { password }, config)
+      .then(res => console.log(res));
+    // axios.put('')
+    this.setState({
+      password: '',
+      password2: ''
+    })
+  }
+
+  handleValidation = (event) => {
+    
+  }
 
   render() {
     return (
       <div className="userinfoContainer">
-        <form className="formContainer">
-          <label>
-            Name:
-            <span className="inputBox1">
-              <input
-                type="text"
-                name="name" // changed this to name from username so that handleChange() can work properly
-                placeholder="Logged in user Name here"
-                value={this.state.name}
-                onChange={this.handleChange}
-              />
-            </span>
-          </label>
-          <label>
-            Email:
-            <span className="inputBox4">
-              <input
-                type="text"
-                name="email"
-                placeholder="Logged in user Email here"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-            </span>
-          </label>
-          <label>
-            Old Password:
-            <span className="inputBox2">
-              <input
-                // will need to check the entered password with the old password we have on file, if they do not match -> deny ability to change it (tell user the password entered is incorrect)
-                type="text"
-                name="oldpassword"
-                placeholder="********"
-                value={this.state.oldpassword}
-                onChange={this.handleChange}
-              />
-            </span>
-          </label>
-          <label>
-            New Password:
-            <span className="inputBox3">
-              <input
-                // will need to check the entered password to see that it passes the criteria for passwords we made in our user model, otherwise prompt user to meet the required criteria
-                type="text"
-                name="newpassword"
-                placeholder="********"
-                value={this.state.newpassword}
-                onChange={this.handleChange}
-              />
-            </span>
-          </label>
-        </form>
-        <button /*onClick={this.handleSubmit}*/ className="saveButton">Save</button>
+        <Card style={styles.cardStyles}>
+          <CardBody>
+            <CardTitle>Change Your Password</CardTitle>
+            <form onSubmit={this.handleSubmit}>
+              <div style={styles.inputGroupStyles}>
+                <p style={styles.labelStyles}>New Password</p>
+                <input
+                  style={styles.inputStyles} 
+                  placeholder='New password...'
+                  value={this.state.password}
+                  onChange={this.handleChange('password')}
+                />
+              </div>
+
+              <div style={styles.inputGroupStyles}>
+                <p style={styles.labelStyles}>Re-Enter New Password</p>
+                <input 
+                  style={styles.inputStyles}
+                  placeholder='Re-enter new'
+                  value={this.state.password2}
+                  onChange={this.handleChange('password2')}
+                />
+              </div>
+              <Button type="submit" color="primary">Save Changes</Button>
+            </form>
+          </CardBody>
+        </Card>
       </div>
     );
   }
