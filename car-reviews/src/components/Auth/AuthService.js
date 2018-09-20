22a563ecc9356d349a22cde7c4fb0eda9e6793e6
Herbert Hodgson <herberthodgson@hotmail.com>
2018-09-20 16:40:38 -0400

    Created an AuthService class to that has methods to check jwt tokens.
    - loggedIn(): checks if a token exists and if not expired returns true.
    
    - isTokenExpired(token): takes a token and checks if it is expired.
    
    - getToken(): retrieves user token from localStorage
    
    - logout() - Clears user token from localStorage
    
    - getProfile(): uses the jwt-decode package to decode token.

---
+++ car-reviews/src/components/Auth/AuthService.js
@@ -0,0 +1,41 @@
+import decode from 'jwt-decode';
+
+export default class AuthService {
+  constructor() {
+    this.getProfile = this.getProfile.bind(this);
+  }
+
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken(); // Getting token from localstorage
+    return !!token && !this.isTokenExpired(token); // if token exists and is not expired, return true.
+  }
+
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      console.log(decode(token));
+      if (decoded.exp < Date.now() / 1000) {
+        // Checking if token is expired.
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('jwt');
+  }
+
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('jwt');
+  }
+
+  getProfile() {
+    // Using jwt-decode npm package to decode the token
+    return decode(this.getToken());
+  }
+}
