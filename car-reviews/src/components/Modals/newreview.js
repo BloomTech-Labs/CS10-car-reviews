162cb096eb5eac1bb0035245b7e59a21d4f46b71
Ashiko124 <greeneselectronics@gmail.com>
2018-09-24 16:16:42 -0600

    Added in the handle drop function

--- car-reviews/src/components/Modals/newreview.js
+++ car-reviews/src/components/Modals/newreview.js
@@ -73,6 +73,34 @@ class NewReviewModal extends Component {
     window.location.reload(); // Need a way for the screen to rerender the changes without me doing it explicitly.
   };
 
+  handleDrop = files => {
+    // Push all the axios request promise into a single array
+    const uploaders = files.map(file => {
+      // Initial FormData
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("tags", `codeinfuse, medium, gist`);
+      formData.append("upload_preset", "ovqvnchc"); // Replace the preset name with your own
+      formData.append("api_key", "425737191539185"); // Replace API key with your own Cloudinary key
+      formData.append("timestamp", (Date.now() / 1000) | 0);
+      
+      // Make an AJAX upload request using Axios (replace Cloudinary URL below with your own)
+      return axios.post("https://api.cloudinary.com/v1_1/autoreveiewforyou/image/upload", formData, {
+        headers: { "X-Requested-With": "XMLHttpRequest" },
+      }).then(response => {
+        const data = response.data;
+        const fileURL = data.secure_url // You should store this URL for future references in your app
+        console.log(data);
+      })
+    });
+  
+    // Once all the files are uploaded 
+    axios.all(uploaders).then(() => {
+      // ... perform after upload is successful operation
+      console.log("request finished", uploaders)
+    });
+  }
+
   render() {
     return (
       <div>
