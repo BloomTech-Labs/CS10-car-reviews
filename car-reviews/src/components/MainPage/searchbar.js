09610e67d8ec04a3c1f838445107b4e13e35c84b
SparksFullStack <csparks@antioch.edu>
2018-09-29 14:24:26 -0700

    added default values for the dropdowns and reversed the years so they start at the most recent

--- car-reviews/src/components/MainPage/searchbar.js
+++ car-reviews/src/components/MainPage/searchbar.js
@@ -74,7 +74,7 @@ class Searchbar extends React.Component {
   componentDidMount() {
     axios.get(`https://databases.one/api/?format=json&select=make&api_key=${API_KEY}`)
       .then(res => {
-        this.setState({ makes: res.data.result});
+        this.setState({ makes: res.data.result });
       })
       .catch(err => {
         console.warn(err);
@@ -99,7 +99,7 @@ class Searchbar extends React.Component {
 
     axios.get(`https://databases.one/api/?format=json&select=year&make_id=${newState.selectedValues.make.makeId}&api_key=${API_KEY}`)
         .then(res => {
-          newState.years = res.data.result
+          newState.years = res.data.result.reverse();
           this.setState(newState);
         })
         .catch(err => console.warn(`There was an error getting the years for that make: \n${err}`));
@@ -231,6 +231,7 @@ class Searchbar extends React.Component {
                 name="make"
                 onChange={this.handleChangeMake}
               >
+              <option>Select a Make</option>
               {this.state.makes.map((make) => {
                 return (
                   <option key={make.make_id}>{make.make}</option>
@@ -243,6 +244,7 @@ class Searchbar extends React.Component {
                 name="year"
                 onChange={this.handleChangeYear}
               >
+              <option>Select a Year</option>
               {this.state.years.map((year) => {
                 return <option key={year.year}>{year.year}</option>
               })}
@@ -253,6 +255,8 @@ class Searchbar extends React.Component {
                 name="model"
                 onChange={this.handleChangeModels}
                 >
+
+                <option>Select a Model</option>
               {this.state.models.map((model) => {
                 return <option key={model.model_id}>{model.model}</option>
                 })}
@@ -264,6 +268,8 @@ class Searchbar extends React.Component {
                 name="trim"
                 onChange={this.handleChangeTrim}
               >
+                <option>Select a Trim</option>
+
               {this.state.trims.map((trim) => {
                 return (
                   <option key={trim.trim_id}>{trim.trim}</option>
