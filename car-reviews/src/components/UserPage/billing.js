21f32aa5a9cbe51f03f2d49d265a54aeb4197e9d
Ashiko124 <greeneselectronics@gmail.com>
2018-10-02 14:09:28 -0600

    Removed rows from billing cards are part of same row now

--- car-reviews/src/components/UserPage/billing.js
+++ car-reviews/src/components/UserPage/billing.js
@@ -37,66 +37,60 @@ class Billing extends Component {
                 <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
                 <Elements>
                     <div className="PurchaseCard">
-    {/*             
-                    <CardNumberElement/>
-                    <CardExpiryElement/>
-                    <CardCVCElement/>
-                    <PostalCodeElement/> */}
-                    <Row> 
+                    
                         <Col md="4">
-                        <Card>
-                        <CardBody>
-                          <CardTitle>1 year unlimited reviews: 40% OFF!</CardTitle>
-                          <form onSubmit={this.handleSubmitPassword}>
-                          
-                            <Button className="button" color="primary" onClick={() => this.onPriceClick('Trusted Car Reviews','1 year unlimited reviews',  6.99)} >1 Year Subscription = 6.99</Button>
-                            <Checkout
-                            name={'Trusted Car Reviews'}
-                            description={'1 year unlimited reviews'}
-                            amount={6.99}
-                            />
-                            </form>
-                        </CardBody>
-                        </Card>
+                            <Card >
+                            <CardBody>
+                              <CardTitle>1 year unlimited reviews: 40% OFF!</CardTitle>
+                              <form onSubmit={this.handleSubmitPassword}>
+
+                                <Button className="button" color="primary" onClick={() => this.onPriceClick('Trusted Car Reviews','1 year unlimited reviews',  6.99)} >1 Year Subscription = 6.99</Button>
+                                <Checkout
+                                name={'Trusted Car Reviews'}
+                                description={'1 year unlimited reviews'}
+                                amount={6.99}
+                                />
+                                </form>
+                            </CardBody>
+                            </Card>
                         </Col>   
-                    </Row> 
-                    <Row> 
+                
+                    
                         <Col md="4">
-                        <Card>
-                        <CardBody>
-                          <CardTitle>6 month unlimited reviews: 33% OFF!</CardTitle>
-                          <form onSubmit={this.handleSubmitPassword}>
-                          
-                          <Button className="button" color="primary" onClick={() => this.onPriceClick('Trusted Car Reviews','6 month unlimited reviews', 3.99)} >6 month Subscription = 3.99</Button>
-                          <Checkout
-                            name={`Trusted Car Reviews`}
-                            description={'6 month unlimited reviews'}
-                            amount={3.99}
-                          />  
-                            </form>
-                        </CardBody>
-                        </Card>
+                            <Card>
+                            <CardBody>
+                              <CardTitle>6 month unlimited reviews: 33% OFF!</CardTitle>
+                              <form onSubmit={this.handleSubmitPassword}>
+
+                              <Button className="button" color="primary" onClick={() => this.onPriceClick('Trusted Car Reviews','6 month unlimited reviews', 3.99)} >6 month Subscription = 3.99</Button>
+                              <Checkout
+                                name={`Trusted Car Reviews`}
+                                description={'6 month unlimited reviews'}
+                                amount={3.99}
+                              />  
+                                </form>
+                            </CardBody>
+                            </Card>
                         </Col>
-                    </Row> 
-                    <Row> 
+                
+                    
                     <Col md="4">
-                        <Card>
-                        <CardBody>
-                          <CardTitle>1 month unlimited reviews</CardTitle>
-                          <form onSubmit={this.handleSubmitPassword}>
-                          
-                          <Button className="button" color="primary" onClick={() => this.onPriceClick('Trusted Car Reviews','1 month unlimited reviews', 0.99)} >1 month Subscription = 0.99</Button>
-                          <Checkout
-                            name={'Trusted Car Reviews'}
-                            description={'1 month unlimited reviews'}
-                            amount={0.99}
-                          />
-                            </form>
-                        </CardBody>
-                        </Card>
-                        </Col>
-                              
-                    </Row> 
+                            <Card>
+                            <CardBody>
+                              <CardTitle>1 month unlimited reviews</CardTitle>
+                              <form onSubmit={this.handleSubmitPassword}>
+
+                              <Button className="button" color="primary" onClick={() => this.onPriceClick('Trusted Car Reviews','1 month unlimited reviews', 0.99)} >1 month Subscription = 0.99</Button>
+                              <Checkout
+                                name={'Trusted Car Reviews'}
+                                description={'1 month unlimited reviews'}
+                                amount={0.99}
+                              />
+                                </form>
+                            </CardBody>
+                            </Card>
+                        </Col>   
+                    
                     </div>
                 </Elements>
                 </StripeProvider>
