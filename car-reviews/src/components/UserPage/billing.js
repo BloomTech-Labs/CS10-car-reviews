848c5dde8ec7c9a9c3987432e376f1ecb8d91399
Ashiko124 <greeneselectronics@gmail.com>
2018-10-02 14:49:00 -0600

    Completed nicer billing page

--- car-reviews/src/components/UserPage/billing.js
+++ car-reviews/src/components/UserPage/billing.js
@@ -32,66 +32,53 @@ class Billing extends Component {
         return(
             <Container className="billing-container">
                 <div className="BillingBox">
-                <h3> Billing </h3>
-                <label> payment info</label>
+                <h3 className="element"> Billing </h3>
+                <label className= "element"> payment info</label>
                 <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
                 <Elements>
-                    <div className="PurchaseCard">
-                    
+                    <Row>
                         <Col md="4">
                             <Card >
                             <CardBody>
                               <CardTitle>1 year unlimited reviews: 40% OFF!</CardTitle>
-                              <form onSubmit={this.handleSubmitPassword}>
-
                                 <Button className="button" color="primary" onClick={() => this.onPriceClick('Trusted Car Reviews','1 year unlimited reviews',  6.99)} >1 Year Subscription = 6.99</Button>
                                 <Checkout
                                 name={'Trusted Car Reviews'}
                                 description={'1 year unlimited reviews'}
                                 amount={6.99}
                                 />
-                                </form>
                             </CardBody>
                             </Card>
                         </Col>   
-                
-                    
                         <Col md="4">
                             <Card>
                             <CardBody>
                               <CardTitle>6 month unlimited reviews: 33% OFF!</CardTitle>
-                              <form onSubmit={this.handleSubmitPassword}>
-
                               <Button className="button" color="primary" onClick={() => this.onPriceClick('Trusted Car Reviews','6 month unlimited reviews', 3.99)} >6 month Subscription = 3.99</Button>
                               <Checkout
                                 name={`Trusted Car Reviews`}
                                 description={'6 month unlimited reviews'}
                                 amount={3.99}
                               />  
-                                </form>
+                             
                             </CardBody>
                             </Card>
                         </Col>
-                
-                    
                     <Col md="4">
                             <Card>
                             <CardBody>
-                              <CardTitle>1 month unlimited reviews</CardTitle>
-                              <form onSubmit={this.handleSubmitPassword}>
-
+                              <CardTitle>1 month unlimited reviews Trial Level</CardTitle>
+                              
                               <Button className="button" color="primary" onClick={() => this.onPriceClick('Trusted Car Reviews','1 month unlimited reviews', 0.99)} >1 month Subscription = 0.99</Button>
                               <Checkout
                                 name={'Trusted Car Reviews'}
                                 description={'1 month unlimited reviews'}
                                 amount={0.99}
                               />
-                                </form>
                             </CardBody>
                             </Card>
                         </Col>   
-                    
-                    </div>
+                    </Row>
                 </Elements>
                 </StripeProvider>
                 </div>
