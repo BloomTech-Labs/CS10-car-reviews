1714dd63e977b975da796156a5adc0d69a803183
Steve <shalomlandsman@gmail.com>
2018-10-03 17:14:12 -0400

    implemented sorting search results, removed owned / rented filtering for now

--- car-reviews/src/components/Modals/resultsmodal.js
+++ car-reviews/src/components/Modals/resultsmodal.js
@@ -95,9 +95,7 @@ class ResultsModal extends Component {
     ///////////////////////////////////////////////
   
   render() {
-    // ** NOTE: You shouldn't try and destructure values that may or may not be on the object, as it will crash the whole app (carImage, for example)
-      const { year, make, model, edition, title, content, score, averageScore, user } = this.props;
-      // const { username } = this.props.user;
+      const { year, make, model, edition, title, content, score, createOn, user } = this.props;
       console.log("RESULTS MODAL PROPS: ", this.props);
       
     return (
