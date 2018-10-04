0198f71c86e86e6a3497dc6bf23d4620c6459228
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-10-04 11:59:25 -0700

    removes navbar icons

--- car-reviews/src/components/MainPage/navbar.js
+++ car-reviews/src/components/MainPage/navbar.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import AuthService from '../Auth/authservice';
-// import home from '../Assets/home.png';
-import flRev from '../Assets/flat_review.png'
-import flBil from '../Assets/flat_billing.png';
-import flSet from '../Assets/flat_settings.png';
 import './navbar.css';
-
 
 const Auth = new AuthService();
 
@@ -26,37 +21,18 @@ class Navbar extends Component {
       return (
         <div className="nc">
           <a className="navbar-item" href="/">
-            {/* <img 
-              src={home} 
-              alt="home-icon" 
-              className="icon"
-            /> */}
             Home
           </a>
     
           <a className="navbar-item" href="/MyReviews">
-            <img 
-              src={flRev} 
-              alt="review-icon" 
-              className="icon"
-            />
             My Reviews
           </a>
     
           <a className="navbar-item" href="/Billing">
-            <img 
-              src={flBil} 
-              alt="billing-icon" 
-              className="icon"
-            />
             Billing
           </a>
+
           <a className="navbar-item" href="/Settings">
-            <img 
-              src={flSet} 
-              alt="settings-icon" 
-              className="icon"
-            />
             Settings
           </a>
         </div>
@@ -65,37 +41,18 @@ class Navbar extends Component {
       return (
         <div className="nc">
           <a className="navbar-item" href="/">
-            {/* <img 
-              src={home} 
-              alt="home-icon" 
-              className="icon"
-            /> */}
             Home
           </a>
     
           <a className="navbar-item" href="/MyReviews">
-            <img 
-              src={flRev} 
-              alt="review-icon" 
-              className="icon"
-            />
             My Reviews
           </a>
     
           <a className="navbar-item" href="/Billing">
-            <img 
-              src={flBil} 
-              alt="billing-icon" 
-              className="icon"
-            />
             Billing
           </a>
+          
           <a className="navbar-item" href="/Settings">
-            <img 
-              src={flSet} 
-              alt="settings-icon" 
-              className="icon"
-            />
             Settings
           </a>
           <a className="navbar-item" href="/" onClick={this.signOut}>
